0db3b873e03f548542594a554a1f93de20b494bb
Ram <ram440163@gmail.com>
2024-03-30 20:55:22 +0530

    complete

---
+++ Pagination/index.js
@@ -0,0 +1,13 @@
+
+
+const container = document.getElementById('container');
+
+
+container.addEventListener('click', () => {
+    window.location.href= 'https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json'
+
+})
+
+
+
+
